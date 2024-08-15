1a9ce7b9ae59668ba2f5d12f05be1447f9c6d3a9
AlfredJonsson <alfjns-1@student.ltu.se>
2024-08-15 17:41:11 +0200

    add html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-        return "första sidan heroku test jao"
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
